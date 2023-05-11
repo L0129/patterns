97855b99e8bf689aae2a874c86ec2fa80bfb504f
LAXMI SHUKLA <132082013+L0129@users.noreply.github.com>
2023-05-11 23:13:25 +0530

    Add files via upload

---
+++ pattern24.java
@@ -0,0 +1,37 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class pattern24 {
+
+	public static void main(String[] args){
+		Scanner obj = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n=obj.nextInt();
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			for(int j=1;j<=2*(n-i);j++) {
+				System.out.print(" ");
+			}
+				for(int j=1;j<=i;j++){
+					System.out.print("*");
+				}
+			System.out.println();
+	   }
+		for(int i=n;i>=1;i--){
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			for(int j=1;j<=2*(n-i);j++) {
+				System.out.print(" ");
+			}
+				for(int j=1;j<=i;j++){
+					System.out.print("*");
+				}
+				System.out.println();
+		}
+		obj.close();
+	}
+}
