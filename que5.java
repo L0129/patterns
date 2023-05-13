69c52c627901da6b97e40934302c5c939afeb461
LAXMI SHUKLA <132082013+L0129@users.noreply.github.com>
2023-05-13 20:56:19 +0530

    Add files via upload

---
+++ que5.java
@@ -0,0 +1,22 @@
+package dsa;
+
+public class que5 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+			for(int i=1;i<=5;i++) {
+				for(int j=1;j<=5;j++) {
+					if((i+j)%2==0) {
+						System.out.print("1"
+								);
+					}else {
+						System.out.print("0");
+					}
+				}
+			
+			System.out.println();
+		}
+	}
+}
